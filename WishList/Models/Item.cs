6d33d4ef1009afef83a6aa9a80c8fbfafd0580a4
radia10 <radia.bouddouh.2e@gmail.com>
2022-05-12 16:48:20 +0200

    ajouter set et get a item

--- WishList/Models/Item.cs
+++ WishList/Models/Item.cs
@@ -1,9 +1,12 @@
-﻿namespace WishList.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WishList.Models
 {
     public class Item
     {
         public int Id { get; set; }
-        
+        [Required]
+        [MaxLength(50)]
         public string Description { get; set; }
 
 
